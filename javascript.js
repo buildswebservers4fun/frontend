68808c32371060582b0caf69f7ea79be7faaf46c
Trent J Punt <punttj@rose-hulman.edu>
2017-02-15 15:41:20 -0500

    bug fixes

--- javascript.js
+++ javascript.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
   updateUserList();
+  loadEvents();
 });
 
 var url = "http://477-38.csse.rose-hulman.edu:8080/UserID/"; 
 var addUrl = "";
 
-$("#submitButton").click(function(){
+var loadEvents = function() {
+  $("#submitButton").click(function(){
 
   var name = $("#nameInput").val();
   var email = $("#emailInput").val();
@@ -15,22 +17,99 @@ $("#submitButton").click(function(){
     "email": email
   };
 
-  var post = $.post(url, data)
+  var post = $.post(url, JSON.stringify(data))
     .done(function(response) {
       updateUserList();
       alert("User created!");
     })
-    .fail(function(){
-      alert("Creating a new user failed!");
+    .fail(function(err){
+      alert("Creating a new user failed: " + err.status + ": " + err.statusText);
     });
-});
+  });
+  
+  $("#userSelect").change(function(){
+    var id = $("userSelect").val();
+    var get = $.get(url+id, function(response) {
+      response = JSON.parse(response);
+      $('editNameInput').val(response.name);
+      $('editEmailInput').val(response.email);
+    });
+  });
+  
+  $("#editUserButton").click(function() {
+    var editName = $("#editNameInput").val();
+    var editEmail = $("#editEmailInput").val();
+    var id = $("#userSelect").val();
+
+    var data = {
+      "name": editName,
+      "email": editEmail
+    };
+
+    $.ajax({
+      type: "PUT",
+      url:url+id,
+      data: JSON.stringify(data)
+    })
+    .done(function(response) {
+      updateUserList();
+      alert("User updated!");
+    })
+    .fail(function(response) {
+      alert("Updating failed!");
+    });
+  });
+
+  $("#deleteUserButton").click(function(){
+    var id = $("#userSelect").val();
+    
+    $.ajax({
+      type: "DELETE",
+      url: url+id
+    })
+    .done(function(response){
+      updateUserList();
+      alert("Successfully deleted!");
+    })
+    .fail(function(response){
+      alert("Delete failed!");
+    });
+  });
+
+  $("#addButton").click(function(){
+    var firstNum = $("#firstNumber").val();
+    var secondNum = $("#secondNumber").val();
+
+    var data = {
+      "first": firstNum,
+      "second": secondNum
+    };
+
+    var post = $.post(addUrl, JSON.Stringify(data))
+      .done(function(response) {
+        response = JSON.parse(response);
+        $("#result").text = response.answer;
+        alert("Addition Succeeded!");
+      })
+      .fail(function(response){
+        alert("Addition failed!");
+      });
+  });
+}
+
 
 var updateUserList = function() {
   var get = $.get(url, function(response) {
+    response = JSON.parse(response);
     $("#usersList").empty();
-    $.each(data, function(i, user) {
+    $("#userSelect").empty();
+    if(response.length === 0){
+      alert("no users!");
+      return;
+    }
+    $.each(response, function(i, user) {
         $("#usersList").append($("<li>"+user.id + ": " + user.name + " " + user.email + "</li>"));
-        $("userSelect").append($("<option>").text(user.id).attr("value", user.id));
+        $("#userSelect").append($("<option>").text(user.id).attr("value", user.id));
     });
   })
   .fail(function(){
@@ -38,66 +117,3 @@ var updateUserList = function() {
   });
 };
 
-$("#userSelect").change(function(){
-  var id = $("userSelect").val();
-  var get = $.get(url+id, function(response) {
-    $('editNameInput').val(response.name);
-    $('editEmailInput').val(response.email);
-  });
-});
-
-
-$("#editUserButton").click(function() {
-  var editName = $("#editNameInput").val();
-  var editEmail = $("#editEmailInput").val();
-  var id = $("#userSelect").val();
-
-  var data = {
-    "name": name,
-    "email": email
-  };
-
-  $.ajax({
-    type: "PUT",
-    url:url+id,
-    data: data
-  }).done(function(response) {
-      updateUserList();
-      alert("User updated!");
-    }).fail(function(response) {
-      alert("Updating failed!");
-    });
-});
-
-$("deleteUserButton").click(function(){
-  var id = $("#userSelect").val();
-  
-  $.ajax({
-    type: "DELETE",
-    url: url+id
-  }).done(function(response){
-      updateUserList();
-      alert("Successfully deleted!");
-    }).fail(function(response){
-      alert("Delete failed!");
-    });
-});
-
-$("#addButton").click(function(){
-  var firstNum = $("#firstNumber").val();
-  var secondNum = $("#secondNumber").val();
-
-  var data = {
-    "first": firstNum,
-    "second": secondNum
-  };
-
-  var post = $.post(addUrl, data)
-    .done(function(response) {
-      $("#result").text = response.answer;
-      alert("Addition Succeeded!");
-    })
-    .fail(function(response){
-      alert("Addition failed!");
-    });
-});
